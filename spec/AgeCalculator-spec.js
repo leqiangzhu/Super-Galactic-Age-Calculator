f8bc61208459e51f1f162c50fa20c05041070d9d
Leqiangzhu（David） <41922877+leqiangzhu@users.noreply.github.com>
2018-10-18 00:31:35 -0700

    Update AgeCalculator-spec.js

--- spec/AgeCalculator-spec.js
+++ spec/AgeCalculator-spec.js
@@ -1,21 +1,47 @@
-import { AgeCalculator } from './AgeCalculator';
+import { AgeCalculator } from '../src/AgeCalculator';
 
-describe('GetMaxDay()', function() {
-
-    it('should test the Max day in that month', function() {
-      let test_day = new Date(2018,10,12);
-      let test_day1 = new Date(1984,2,12);
-      expect(test_day.GetMaxDay()).toEqual(31);
-      expect(test_day1.GetMaxDay()).toEqual(29);
-    });
-  
+describe("AgeCalculator", function(){
+  let test_day = new AgeCalculator(2010,1,1);
+  it('should identify a AgeCalculator class',function(){
+   
+    expect(test_day.GetMaxDay()).toEqual(31);
   });
 
-  describe('GetMaxDay()', function() {
 
-    it('should test the Max day in that month', function() {
-      let test_day = new Date(2018,10,12);
-      expect(test_day.GetMaxDay()).toEqual(31);
-    });
-  
+  it('should check if the GetAgeOnEarth() works',function(){
+    //the data is from https://www.calculator.net/age-calculator.html
+    expect(test_day.GetAgeOnEarth()).toEqual([8,9,16]);
+    expect(test_day.GetAgeOnEarth()).not.toEqual([17,10,28]);
   });
+  
+
+  it('should check if the GetAgeOnMercury() works',function(){
+    expect(test_day.GetAgeOnMercury()).toEqual("36.7");
+  });
+
+  it('should check if the GetAgeOnVenus() works',function(){
+    expect(test_day.GetAgeOnVenus()).not.toEqual(75);
+  });
+
+  it('should check if the GetAgeOnMars() works',function(){
+    expect(test_day.GetAgeOnMars()).toEqual("4.7");
+  });
+
+  it('should check if the GetAgeOnJupiter() works',function(){
+    expect(test_day.GetAgeOnJupiter()).toEqual("0.7");
+  });
+
+  it('should check if the GetLeftYearsEarth() works',function(){
+    let test_day = new AgeCalculator(2010,1,1);
+    expect(test_day.GetLeftYearsEarth()).toEqual(70);
+  });
+
+ 
+  // it('should check if the GetAgeOnMercury() works',function(){
+  //   let test_day = new AgeCalculator(2000,10,20);
+  //   expect(test_day.GetAgeOnMercury()).not.toEqual(75);
+  // });
+
+
+  
+});
